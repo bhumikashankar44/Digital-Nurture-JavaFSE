455ea8884a3bdb4b7a5e203273f38c654367545b
bhumikashankar44 <bhumikashankar6@gmail.com>
2026-06-21 21:11:14 +0530

    Implement Singleton Logger class in Main.java

---
+++ FactoryMethodPattern/Main.java
@@ -0,0 +1,32 @@
+class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger instance created");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("User Logged In");
+
+        System.out.println("Same Instance? " + (logger1 == logger2));
+    }
+}
